fd48b2d480bc60c347b5a950f751008980097a5e
arlops22 <ary.s.lopes22@gmail.com>
2026-09-11 00:24:47 -0300

    feat: header with logo inside default layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from 'next';
-import { DM_Sans } from 'next/font/google';
+import Link from 'next/link';
+import Image from 'next/image';
+import { DM_Sans, Inter } from 'next/font/google';
+
+import logoImg from '../public/images/logo.png';
 import './globals.css';
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const dmSans = DM_Sans();
 
@@ -11,8 +18,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
     return (
-        <html lang="en" className={`h-full antialiased bg-dark-gray-700 text-dark-gray-100 ${dmSans.className}`}>
+        <html lang="en" className={cn("h-full", "antialiased", "bg-dark-gray-700", "text-dark-gray-100", dmSans.className, "font-sans", inter.variable)}>
             <body suppressHydrationWarning className="min-h-full flex flex-col">
+                <header>
+                    <div className="container py-10 mx-auto">
+                        <Link href={'/'}>
+                            <Image alt="Logo" src={logoImg} />
+                        </Link>
+                    </div>
+                </header>
                 {children}
             </body>
         </html>
